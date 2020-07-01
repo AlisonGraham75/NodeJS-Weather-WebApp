f13169628c5b5dece1b579f56d1957f97f37b0b5
mb2081 <alison.graham@gdit.hcqis.org>
2020-07-01 14:39:39 -0600

    Added a Search Form and an event listener for it

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,29 @@
 //On client side .js these console log show in browser tools console
 console.log('Client side javascript file is loaded')
 
-//Fetch can only be used in client side js like this.
-//fetch data from this url and then run this method
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    
-    //then section gets called when json data arrives and has been parsed.
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+
+
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    //prevents auto refresh in browser
+    event.preventDefault()
+
+    const location = searchInput.value
+
+    //Fetch can only be used in client side js like this.
+    //fetch data from this url and then run this method
+    fetch('http://localhost:3000/weather?city=' + location).then((response) => {
+        
+        //then section gets called when json data arrives and has been parsed.
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
